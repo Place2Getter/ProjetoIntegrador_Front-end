06ee6b012924ed31546dc2e6919884ba481a8efe
Luis Henrique Local <luish8808@gmail.com>
2021-08-05 08:50:50 -0300

    Atualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { ContatoComponent } from './contato/contato.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'home', pathMatch: 'full'},
   {path: 'contato', component: ContatoComponent},
   {path: 'logar', component: LoginComponent},
   {path: 'cadastrar', component: CadastroComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'inicio', component: InicioComponent}
 ];
 
 @NgModule({
